40a03ac3fd28874ec348f684fad2dec1166f8f82
Artem Vovk <volkodav4ik1990@gmail.com>
2020-03-25 14:47:20 +0200

    Add task 2*

--- src/main/java/com/volkodav4ik/Group.java
+++ src/main/java/com/volkodav4ik/Group.java
@@ -3,11 +3,31 @@ package com.volkodav4ik;
 import java.util.List;
 
 public class Group {
+
     private String name;
     private List<Person> persons;
 
+    public Group() {
+    }
+
     public Group(String name, List<Person> persons) {
         this.name = name;
+        this.persons = persons;
+    }
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPersons(List<Person> persons) {
         this.persons = persons;
     }
 
--- src/main/java/com/volkodav4ik/Main.java
+++ src/main/java/com/volkodav4ik/Main.java
@@ -1,26 +1,41 @@
 package com.volkodav4ik;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class Main {
     private static final String SAVE_FILE_NAME = "save.txt";
+    private static final String XML_FILE_NAME = "xmlSample.xml";
     private static Group group;
 
     public static void main(String[] args) {
         gsonFromFile();
         System.out.println(group.toString());
+        serializeToXmlByJackson();
     }
-    private static void gsonFromFile(){
-        try (BufferedReader reader = new BufferedReader(new FileReader(SAVE_FILE_NAME))){
+
+    private static void serializeToXmlByJackson() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(new File(XML_FILE_NAME), group);
+        } catch (IOException e) {
+            System.out.println("File hasn't created!");
+        }
+    }
+
+    private static void gsonFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(SAVE_FILE_NAME))) {
             String strForParsing = reader.readLine();
             Gson gson = new GsonBuilder().create();
             group = gson.fromJson(strForParsing, Group.class);
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("File hasn't been found!");
         }
     }
